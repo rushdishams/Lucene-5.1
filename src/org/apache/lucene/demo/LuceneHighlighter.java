ea983d6333bdb8514c7c180d7a38c8d09a82f44a
rushdishams <rushdecoder@gmail.com>
2015-05-25 19:55:01 +0000

    first commit from moreover

---
+++ src/org/apache/lucene/demo/LuceneHighlighter.java
@@ -0,0 +1,57 @@
+package org.apache.lucene.demo;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+ 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.NullFragmenter;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+ 
+public class LuceneHighlighter{
+    public static String highlight(String pText, String pQuery) throws Exception{	
+    	
+    	
+    	SimpleHTMLFormatter htmlFormatter = new SimpleHTMLFormatter("<span style='background-color: #ffffcc'>", "</span>");
+    	Analyzer analyzer = new StandardAnalyzer();
+    	QueryParser parser = new QueryParser("contents", analyzer);
+	
+        Highlighter highlighter = new Highlighter(htmlFormatter, new QueryScorer(parser.parse(pQuery)));
+        highlighter.setTextFragmenter(new NullFragmenter());
+ 
+        String text = highlighter.getBestFragment(analyzer, "", pText); 
+ 
+        if (text != null){
+            return text;
+        }
+        return pText;    
+    }
+ 
+    public static void main(String[] args){
+
+        	File file = new File ("filesToIndex/AntioxidantReport.txt");
+        	String text = null;
+
+        	try {
+				text = FileUtils.readFileToString(file);
+			} catch (IOException e) {
+				System.out.println("Error reading file");
+			}
+        	
+            try {
+				System.out.println(highlight(text, "management"));
+            	//highlight(text, "management");
+			} catch (Exception e) {
+				System.out.println("Error highlighting text");
+			}
+       
+    }// end main
+
+    
+}//end class
